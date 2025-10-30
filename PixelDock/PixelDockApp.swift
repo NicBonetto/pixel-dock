5edb000c4daa5510f55aa1bcb8729ac61bbbfd3c
Nic Bonetto <nick.bonetto@gmail.com>
2025-10-30 10:53:26 -0700

    Initial Commit

---
+++ PixelDock/PixelDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelDockApp.swift
+//  PixelDock
+//
+//  Created by Nicholas Bonetto on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct PixelDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
